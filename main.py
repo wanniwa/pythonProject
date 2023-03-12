fbb27ffc69674554dfac0571b63b77611c8d1b79
wanniwa <734933034@qq.com>
2023-03-12 23:24:18 +0800

    批量

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
-import argparse
 import re
 import json
 import os
-# file_path = '/Users/wangning/Downloads/andre_route_p.rpy'
-
 import sys
 
+def process_file(file_path):
+    # 读取数据文件
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = f.read()
+
+    # 提取数据
+    pattern = r'translate schinese (?P<key>cutscene_\w+):\n\n\s+# (?P<english>.+)\n\s+(?P<chinese>.+)\n'
+    matches = re.findall(pattern, data)
+
+    # 转换为JSON格式
+    output = []
+    for match in matches:
+        output.append({
+            'key': match[0],
+            'original': match[1],
+            'translation': match[2]
+        })
+
+    output_json = json.dumps(output, indent=4, ensure_ascii=False)
+
+    # 输出JSON数据
+    print(output_json)
+    output_file_path = os.path.splitext(file_path)[0] + '.json'
+    with open(output_file_path, 'w', encoding='utf-8') as f:
+        f.write(output_json)
+
 # 通过命令行参数获取文件路径
-file_path = sys.argv[1]
+file_paths = sys.argv[1:]
 
-# 读取数据文件
-with open(file_path, 'r', encoding='utf-8') as f:
-    data = f.read()
-
-# 提取数据
-pattern = r'translate schinese (?P<key>cutscene_\w+):\n\n\s+# (?P<english>.+)\n\s+(?P<chinese>.+)\n'
-matches = re.findall(pattern, data)
-
-# 转换为JSON格式
-output = []
-for match in matches:
-    output.append({
-        'key': match[0],
-        'original': match[1],
-        'translation': match[2]
-    })
-
-output_json = json.dumps(output, indent=4, ensure_ascii=False)
-
-# 输出JSON数据
-print(output_json)
-output_file_path = os.path.splitext(file_path)[0] + '.json'
-with open(output_file_path, 'w', encoding='utf-8') as f:
-    f.write(output_json)
+for file_path in file_paths:
+    process_file(file_path)
--- read.py
+++ read.py
@@ -3,20 +3,27 @@ import json
 import os
 import sys
 
-file_path = sys.argv[1]
-with open(file_path, 'r', encoding="utf-8") as f:
-    data = json.load(f)
 
-# 转换为原始格式
-output = ''
-for item in data:
-    output += f'translate schinese {item["key"]}:\n\n'
-    output += f'    # {item["original"]}\n'
-    output += f'    {item["translation"]}\n'
+def process_file(file_path):
+    with open(file_path, 'r', encoding="utf-8") as f:
+        data = json.load(f)
 
-# 输出原始格式
-print(output)
-# 输出JSON数据
-output_file_path = os.path.splitext(file_path)[0] + '_chinese.rpy'
-with open(output_file_path, 'w', encoding="utf-8") as f:
-    f.write(output)
+    # 转换为原始格式
+    output = ''
+    for item in data:
+        output += f'translate schinese {item["key"]}:\n\n'
+        output += f'    # {item["original"]}\n'
+        output += f'    {item["translation"]}\n'
+
+    # 输出原始格式
+    print(output)
+    # 输出JSON数据
+    output_file_path = os.path.splitext(file_path)[0] + '.rpy'
+    with open(output_file_path, 'w', encoding="utf-8") as f:
+        f.write(output)
+
+
+file_paths = sys.argv[1:]
+
+for file_path in file_paths:
+    process_file(file_path)
